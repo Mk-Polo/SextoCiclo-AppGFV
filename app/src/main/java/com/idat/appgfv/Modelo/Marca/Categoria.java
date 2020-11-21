06f89fb3fe385bc4f91db0468ac2f642a9f041ee
ivan <73268643+a18103812@users.noreply.github.com>
2020-11-21 00:12:53 -0500

    categoria

---
+++ app/src/main/java/com/idat/appgfv/Modelo/Marca/Categoria.java
@@ -0,0 +1,30 @@
+package com.idat.appgfv.Modelo.Marca;
+
+public class Categoria {
+    private Long id;
+    private String categoria;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String categoria) {
+        this.id = id;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
